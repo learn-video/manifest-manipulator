9bdd804f90815f845303157c3b50efe76167b925
Maurício Antunes <mauricio.abreua@gmail.com>
2023-07-09 08:41:50 -0300

    feat: Reduce max bandwidth (demo)

--- demo/main.go
+++ demo/main.go
@@ -7,7 +7,27 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
+
+	"github.com/learn-video/manifest-manipulator/filter"
 )
+
+func modifyManifest(res *http.Response) (io.Reader, error) {
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if strings.HasSuffix(res.Request.URL.Path, "/master.m3u8") {
+		p, err := filter.NewMasterPlaylist(*bytes.NewBuffer(body))
+		if err != nil {
+			return nil, err
+		}
+		p.FilterBandwidth(filter.BandwidthFilter{Max: 3000000})
+
+		body = []byte(p.Playlist.String())
+	}
+	return bytes.NewReader(body), nil
+}
 
 func main() {
 	exit := make(chan bool)
@@ -21,7 +41,7 @@ func main() {
 		http.ListenAndServe(":8080", handler)
 	}()
 
-	targetURL := "https://test-streams.mux.dev"
+	targetURL := "https://cph-p2p-msl.akamaized.net"
 	target, err := url.Parse(targetURL)
 	if err != nil {
 		log.Fatal(err)
@@ -37,11 +57,13 @@ func main() {
 		res.Header.Set("Access-Control-Allow-Origin", "*")
 		res.Header.Set("Access-Control-Allow-Methods", "*")
 		res.Header.Set("Access-Control-Allow-Headers", "*")
-		body, err := io.ReadAll(res.Body)
+
+		body, err := modifyManifest(res)
 		if err != nil {
 			return err
 		}
-		res.Body = io.NopCloser(bytes.NewReader(body))
+
+		res.Body = io.NopCloser(body)
 		return nil
 	}
 	proxy := &httputil.ReverseProxy{
